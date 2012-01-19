b95c6586befa44472d20ac58fd9390a48ec6358a
Ilshad Khabibullin <astoon@spacta.com>
2012-01-19 08:19:05 +0400

    first tests OK

--- defpage/meta/__init__.py
+++ defpage/meta/__init__.py
@@ -1,7 +1,7 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
-from defpage.auth.sql import initialize_sql
-from defpage.auth.config import system_params
+from defpage.meta.sql import initialize_sql
+from defpage.meta.config import system_params
 
 def main(global_config, **settings):
     system_params.update(settings)
--- defpage/meta/sql.py
+++ defpage/meta/sql.py
@@ -18,7 +18,7 @@ class Collection(Base):
 
     __tablename__ = "collections"
 
-    collection_id = Coluemn(Integer, primary_key=True, autoincrement=False)
+    collection_id = Column(Integer, primary_key=True, autoincrement=False)
 
     title = Column(Unicode)
 
