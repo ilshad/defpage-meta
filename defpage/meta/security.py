8c8a52bb23ab22cd403b991fd8c361e38ddfd554
Ilshad Khabibullin <astoon@spacta.com>
2012-01-25 14:47:52 +0400

    fix security

--- defpage/meta/security.py
+++ defpage/meta/security.py
@@ -4,14 +4,15 @@ from defpage.meta.sql import CollectionUserRole
 from defpage.meta.interfaces import ICollection
 
 def security_checker(credentials, request):
-    userid = credentials["login"]
     roles = []
-    c = ICollection(request.context, None)
-    if c:
-        cur = DBSession().query(CollectionUserRole).filter(
-            and_(CollectionUserRole.collection_id==c.collection_id,
-                 CollectionUserRole.user_id==userid)
-            ).first()
-        if cur:
-            roles.append(cur.role)
+    userid = credentials["login"]
+    if userid:
+        c = ICollection(request.context, None)
+        if c:
+            cur = DBSession().query(CollectionUserRole).filter(
+                and_(CollectionUserRole.collection_id==c.collection_id,
+                     CollectionUserRole.user_id==userid)
+                ).first()
+            if cur:
+                roles.append(cur.role)
     return roles
