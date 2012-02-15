cb4da000f7cea66f3b74db632ac46c75488dee46
Ilshad Khabibullin <astoon@spacta.com>
2012-02-15 23:15:26 +0400

    fix POST

--- defpage/meta/__init__.py
+++ defpage/meta/__init__.py
@@ -23,7 +23,7 @@ def main(global_config, **settings):
     config.add_route("doc", "/documents/{name}", factory=get_document, custom_predicates=(is_int,))
 
     config.add_view("defpage.meta.views.add_collection", route_name="cols", renderer="json", request_method="POST")
-    config.add_view("defpage.meta.views.edit_collection", route_name="col", request_method="POSTT", permission="manage")
+    config.add_view("defpage.meta.views.edit_collection", route_name="col", request_method="POST", permission="manage")
     config.add_view("defpage.meta.views.del_collection", route_name="col", request_method="DELETE", permission="delete")
     config.add_view("defpage.meta.views.get_collection", route_name="col", renderer="json", request_method="GET", permission="view")
     config.add_view("defpage.meta.views.search_collections", route_name="cols", renderer="json", request_method="GET")
