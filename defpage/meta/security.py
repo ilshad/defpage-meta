87ff0bbf29be2fced23b901f1b50768a02aa58ff
Ilshad Khabibullin <astoon@spacta.com>
2012-02-23 06:12:45 +0400

    introduced system user

--- defpage/meta/security.py
+++ defpage/meta/security.py
@@ -2,17 +2,22 @@ from sqlalchemy import and_
 from defpage.meta.sql import DBSession
 from defpage.meta.sql import CollectionUserRole
 from defpage.meta.interfaces import ICollection
+from defpage.meta.config import system_params
+from defpage.meta import roles
 
 def security_checker(credentials, request):
     roles = []
     userid = credentials["login"]
     if userid:
-        c = ICollection(request.context, None)
-        if c:
-            cur = DBSession().query(CollectionUserRole).filter(
-                and_(CollectionUserRole.collection_id==c.collection_id,
-                     CollectionUserRole.user_id==userid)
-                ).first()
-            if cur:
-                roles.append(cur.role)
+        if userid == system_params["system_user"]:
+            roles.append(roles.OWNER)
+        else:
+            c = ICollection(request.context, None)
+            if c:
+                cur = DBSession().query(CollectionUserRole).filter(
+                    and_(CollectionUserRole.collection_id==c.collection_id,
+                         CollectionUserRole.user_id==userid)
+                    ).first()
+                if cur:
+                    roles.append(cur.role)
     return roles
