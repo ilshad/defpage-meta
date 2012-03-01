ce5f32d271d3d8355822b758549027b24abfbef3
Ilshad Khabibullin <astoon@spacta.com>
2012-03-02 02:39:07 +0400

    docs modified datetime in collection - in rfc3339 now

--- defpage/meta/util.py
+++ defpage/meta/util.py
@@ -18,7 +18,7 @@ def dict_list_required(v):
         raise HTTPBadRequest
     return [dict_required(i) for i in v]
 
-DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 def datetime_format(dt):
     return dt.strftime(DATETIME_FORMAT)
