d6922fe7d7a5b17912652d57371f880d4f8ba56d
Ilshad Khabibullin <astoon@spacta.com>
2012-03-17 17:09:49 +0400

    min fix

--- defpage/meta/source.py
+++ defpage/meta/source.py
@@ -1,5 +1,9 @@
-ATTRIBUTES = {"gd": {"source": ["access_token", "refresh_token", "token_expiry"],
-                "collection": ["folder_id"]}}
+# data schema for `source_details` fields in database
+# in `sources` and `collections` tables.
+ATTRIBUTES = {"gd": {"source": ["access_token",
+                                "refresh_token",
+                                "token_expiry"],
+                     "collection": ["folder_id"]}}
 
 def make_details(stype, sname, sfrom):
     return dict((x, sfrom.get(x)) for x in ATTRIBUTES[stype][sname])
