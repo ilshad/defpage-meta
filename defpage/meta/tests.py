87c28221059ed0f551c1d817ba2e99cd589c1559
Ilshad Khabibullin <astoon@spacta.com>
2012-10-21 15:31:21 +0400

    func tests: create collection

--- defpage/meta/tests.py
+++ defpage/meta/tests.py
@@ -1,5 +1,8 @@
+# coding: utf-8
+
 import unittest
 import transaction
+import base64
 from pyramid import testing
 
 def _testingDB():
@@ -22,7 +25,7 @@ class UnitTests(unittest.TestCase):
     def setUp(self):
         request = testing.DummyRequest()
         self.config = testing.setUp(request=request)
-        self.dbs = _testingDB()
+        #self.dbs = _testingDB()
 
     def tearDown(self):
         testing.tearDown()
@@ -30,7 +33,11 @@ class UnitTests(unittest.TestCase):
     def test_quick(self):
         pass
 
-SETTINGS = {'sqlalchemy.url':'sqlite://'}
+SETTINGS = {'sqlalchemy.url':'sqlite://',
+            'system.system_user':'system'}
+
+def _basic_user(userid):
+    return "Basic " + base64.b64encode(str(userid) + ":1")
 
 class FuncTests(unittest.TestCase):
 
@@ -38,7 +45,59 @@ class FuncTests(unittest.TestCase):
         from defpage.meta import main
         app = main({}, **SETTINGS)
         from webtest import TestApp
-        self.testapp = TestApp(app)
+        self.app = TestApp(app)
 
-    def test_root(self):
-        res = self.testapp.get('/', status=404)
+    def tearDown(self):
+        self.app.reset()
+
+    def test_functional(self):
+        r = self.app.get('/', status=404)
+
+        # get collections info
+        r = self.app.get('/collections/',
+                         params={'info':'total_num'})
+        self.assertEqual(r.json['total_num'], 0)
+
+        r = self.app.get('/collections/',
+                         params={'info':'max_id'})
+        self.assertEqual(r.json['max_id'], None)
+
+        # create collection
+        r = self.app.post_json('/collections/',
+                               params={'title':u'Первая коллекция',
+                                       'owner':1},
+                               status=201)
+        self.assertEqual(r.json['id'], 1)
+
+        r = self.app.post_json('/collections/',
+                               params={'title':u'Вторая коллекция',
+                                       'owner':1},
+                               status=201)
+        self.assertEqual(r.json['id'], 2)
+
+        # get collections info
+        r = self.app.get('/collections/',
+                         params={'info':'total_num'})
+        self.assertEqual(r.json['total_num'], 2)
+
+        r = self.app.get('/collections/',
+                         params={'info':'max_id'})
+        self.assertEqual(r.json['max_id'], 2)
+
+        r = self.app.get('/collections/',
+                         params={'user_id':1})
+        self.assertEqual(type(r.json), list)
+
+        self.assertEqual(r.json[0]['id'], 1)
+        self.assertEqual(r.json[0]['title'], u'Первая коллекция')
+        self.assertEqual(r.json[0]['role'], 'owner')
+
+        self.assertEqual(r.json[1]['id'], 2)
+        self.assertEqual(r.json[1]['title'], u'Вторая коллекция')
+        self.assertEqual(r.json[1]['role'], 'owner')
+
+        # get collection
+        r = self.app.get('/collections/1',
+                         headers={'Authorization':_basic_user(1)})
+        self.assertEqual(r.json['title'], u'Первая коллекция')
+        #self.assertEqual(r.json['source'], '')
